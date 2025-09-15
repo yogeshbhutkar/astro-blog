8930f4fcc727b847a56bc7bd0e533b4455ebce35
yogeshbhutkar <yogesh.bhutkar@rtcamp.com>
2025-09-15 09:05:36 +0530

    feat: add sitemaps and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,11 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import path from 'path'
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+	site: process.env.SITE_URL || 'https://localhost:4321',
 	vite: {
 		plugins: [tailwindcss()],
 		resolve: {
@@ -13,5 +16,6 @@ export default defineConfig({
 				'@': path.resolve('./src'),
 			}
 		}
-	},
+		},
+	integrations: [sitemap()],
 });
