86b4305ab46c7fa89a2a28537e91f705c0f39869
yogeshbhutkar <yogesh.bhutkar@rtcamp.com>
2025-09-16 09:58:15 +0530

    feat: add mdx support

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,16 +6,29 @@ import path from 'path'
 
 import sitemap from '@astrojs/sitemap';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
-	site: process.env.SITE_URL || 'https://localhost:4321',
-	vite: {
-		plugins: [tailwindcss()],
-		resolve: {
-			alias: {
-				'@': path.resolve('./src'),
+    site: process.env.SITE_URL || 'https://localhost:4321',
+    vite: {
+        plugins: [tailwindcss()],
+        resolve: {
+            alias: {
+                '@': path.resolve('./src'),
+            }
+        }
+        },
+    integrations: [
+		sitemap(),
+		mdx({
+			syntaxHighlight: 'shiki',
+			shikiConfig: {
+				themes: {
+					light: 'catppuccin-latte',
+					dark: 'tokyo-night',
+				},
 			}
-		}
-		},
-	integrations: [sitemap()],
+		})
+	],
 });
