9df63ad483e0ad043215d0f55aa6ff87d6554d2b
yogeshbhutkar <yogesh.bhutkar@rtcamp.com>
2025-09-21 13:50:59 +0530

    feat(rss): add rss feed

---
+++ src/constants.ts
@@ -0,0 +1,12 @@
+/**
+ * Site constants
+ */
+
+export const LOCAL_SITE_URL = "http://localhost:4321";
+
+export const SITE_TITLE = "Yogesh's Blog";
+
+export const SITE_DESCRIPTION =
+	"I’m Yogesh, a developer who loves to learn, build, and share. This blog captures my coding journey, problem-solving approaches, and experiments with modern tools and technologies.";
+
+export const SITE_OG_IMAGE_PATH = "images/og-image.webp";
