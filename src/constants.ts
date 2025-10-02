14b15d8416412cbb62554786a682904ae8f5444b
yogeshbhutkar <yogesh.bhutkar@rtcamp.com>
2025-10-02 12:50:42 +0530

    fix: error infering cover image types

--- src/constants.ts
+++ src/constants.ts
@@ -10,3 +10,6 @@ export const SITE_DESCRIPTION =
 	"I’m Yogesh, a developer who loves to learn, build, and share. This blog captures my coding journey, problem-solving approaches, and experiments with modern tools and technologies.";
 
 export const SITE_OG_IMAGE_PATH = "images/og-image.webp";
+
+export const DEFAULT_COVER_IMAGE_WIDTH = 1024;
+export const DEFAULT_COVER_IMAGE_HEIGHT = 512;
