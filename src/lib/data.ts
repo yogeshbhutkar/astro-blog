bbbd2ca0be6ff0fc0c780861c94572b2c884faf3
yogeshbhutkar <yogesh.bhutkar@rtcamp.com>
2025-10-20 12:47:27 +0530

    feat: add personal info

---
+++ src/lib/data.ts
@@ -0,0 +1,84 @@
+export const CURRENT_WORK_EXPERIENCE =
+	"I'm currently working as a Software Engineer at rtCamp, where I focus on developing full-stack solutions using React, Node.js, and WordPress. My role involves architecting scalable web applications, contributing to open-source projects, and collaborating with cross-functional teams to deliver high-performance digital experiences.";
+
+export const WORK_EXPERIENCE = [
+	{
+		company: "rtCamp",
+		position: "Software Engineer",
+		startDate: "Jul 2024",
+		endDate: "Current",
+		location: "Pune, India (Remote)",
+		url: "https://rtcamp.com/",
+		excerpt:
+			"At rtCamp, I develop full-stack solutions leveraging React, Node.js, and WordPress to architect scalable, high-performance web applications. I regularly contribute to open-source projects and collaborate with cross-functional teams to deliver client-focused digital experiences.",
+		responsibilities: [
+			"Contributed to the open-source React-based Gutenberg editor, core to WordPress, which powers 43% of the web. Delivered feature enhancements, bug fixes, and maintained Jest and Playwright tests. Recognized as a noteworthy contributor in the WP 6.8 release.",
+			"Architected and developed a scalable LLM pipeline, leveraging the FastAPI framework and Pub/Sub architecture to process large-scale data and enable real-time model interaction.",
+		],
+	},
+	{
+		company: "Metaverse Ventures Pvt. Ltd.",
+		position: "Full Stack Developer",
+		startDate: "Oct 2023",
+		endDate: "Jun 2024",
+		location: "Hyderabad, India (Remote)",
+		url: "https://www.m0.ventures/",
+		excerpt: null,
+		responsibilities: [
+			"Architected and developed an AI-powered word editor using TipTap and large language models, enabling brand-specific content generation through dynamic digital personas and tone-consistent writing assistance.",
+			"Built and deployed a scalable LLM-driven outreach pipeline using NestJS and Kafka, automating personalized bulk email campaigns with dynamically populated content and multi-step flows based on recipient interactions.",
+		],
+	},
+	{
+		company: "Freelance",
+		position: "Next.JS Developer",
+		startDate: "Sept 2023",
+		endDate: "Oct 2023",
+		location: "Remote",
+		url: "https://hittavard.se/",
+		excerpt: null,
+		responsibilities: [
+			"Developed a responsive, SEO-optimized healthcare website using Next.js and Tailwind CSS.",
+			"Enhanced visibility using sitemaps and structured data.",
+			"Implemented internationalization (i18n) for multilingual support.",
+		],
+	},
+	{
+		company: "Pupilfirst Pvt. Ltd.",
+		position: "Full Stack Developer Intern",
+		startDate: "Apr 2023",
+		endDate: "Sept 2023",
+		location: "Remote",
+		url: "https://pupilfirst.com/",
+		excerpt: null,
+		responsibilities: [
+			"Worked alongside eGov Foundation and Open Health Care to digitise hospital data, deploying services across 6+ Indian states and enabling patient monitoring in remote areas.",
+			"Enhanced the web app’s usability and functionality by developing and integrating custom ReactJS UI components, improving workflows for hundreds of healthcare professionals.",
+			"Contributed to a NextJS application integrating a Large Language Model with a RAG pipeline to deliver context-aware, critical healthcare responses.",
+		],
+	},
+];
+
+export const PROJECTS = [
+	{
+		name: "Kiri 🌿",
+		description:
+			"A minimalistic personal blogging space built with Astro and Tailwind CSS. Comes with light & dark mode, and available as a public template.",
+		url: "https://kiri-ten.vercel.app/",
+		repo: "https://github.com/yogeshbhutkar/kiri",
+	},
+	{
+		name: "ChatOn",
+		description:
+			"A chat application built with Flutter and Firebase for real-time messaging.",
+		url: undefined,
+		repo: "https://github.com/yogeshbhutkar/ChatOn",
+	},
+	{
+		name: "Node.js reverse proxy server",
+		description:
+			"A simple Node.js reverse proxy prototype inspired by NGINX, designed for forwarding HTTP requests with minimal setup.",
+		url: undefined,
+		repo: "https://github.com/yogeshbhutkar/node-reverse-proxy",
+	},
+];
